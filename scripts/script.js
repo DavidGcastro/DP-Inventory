c02c807d0af49c003f8b294d317aa2f5af5bae86
David <davidgcastro93@gmail.com>
2017-09-21 22:54:41 -0400

    finished styling for desktop breakpoint. Needs JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
-  var container = document.querySelector('#masonry');
+//for masonry
+
+var container = document.querySelector('#masonry');
   var masonry = new Masonry(container, {
-    columnWidth: 10,
+    columnWidth: 5,
     itemSelector: '.item'
   });
+
+
+//Sidefilter logic
+
+
