18c8ee3abb2bb240523abde50fb5e6825fa0de43
David <davidgcastro93@gmail.com>
2017-09-24 15:47:52 -0400

    need to add logic to make all images display when they are all hidden

--- scripts/script.js
+++ scripts/script.js
@@ -7,55 +7,45 @@ var masonry = new Masonry(container, {
 //Sidefilter logic
 $(document).ready(function() {
 	$(".sidefilter-item").on("click", function() {
-        //adds background to side filter items when clicked
+		//adds background to side filter items when clicked
 		$(this).addClass("side-filter-item_selected");
-        // adds an icon to the side filter item selected
+		// adds an icon to the side filter item selected
 		$(this).find('.close').addClass("fa fa-times");
 		var chosenFilter = $(this).attr('id');
-        
+		//loop thru images and hide elements that do not share the chosenFilter name
 		
-        //loop thru images and hide elements that do not share the chosenFilter name
-		$("img").each(function() {
-			var currentImg = this;
-            
-            //hide img elements that DO NOT have the class of the sidefilter selected
-			if ($(currentImg).hasClass(chosenFilter)) {
-                $("."+chosenFilter).removeClass('hide');
-            	$(this).removeClass('hide');
+        
+        
+        $('img').each(function() {
+			//target classes that have side filter class
+			if ($(this).hasClass(chosenFilter)) {
+				$(this).addClass('chosen');
 			}
-            else{
+			if ($(this).hasClass('chosen')) {
+				$(this).removeClass('hide');
+			} 
+            else {
 				$(this).addClass('hide');
-            }
-          //remove hide of an image when the appropriate filter has been chosen.
-          //trigger masonry
-         
+			}
 		});
-        
-        $('.close').on('click', function(){
-             // chose side filter
-             var parent = $(this).parent().attr('id');
-             var currentImg = this;
-             $('img').each(function(){
-                 //target classes that have side filter class
-                 if(!$(this).hasClass(parent) && $(this).hasClass("hide") ){
-                   //make them appear
-                   $(this).removeClass('hide');
-                 }
-             });
-
-        });
-       
-      
-        
-        
-	});
-        //displays active state and close icon
-    	$('.close').click(function(e) {
+		//displays active state and close icon
+		$('.close').click(function(e) {
 			e.stopPropagation();
-            //removes icon when clicking icon
+			//removes icon when clicking icon
 			$(this).removeClass('fa fa-times');
-            // removes clicked state when clicking icon
+			// removes clicked state when clicking icon
 			$(this).parent().removeClass("side-filter-item_selected");
-   
+			var parent = $(this).parent().attr('id');
+			
+            $('img').each(function() {
+				if ($(this).hasClass(parent)) {
+					$(this).removeClass('chosen');
+					$(this).addClass('hide');
+				}
+              
+			
+			});
 		});
+		
+	});
 });
