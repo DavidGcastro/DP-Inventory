7fdebc53018d756f6cf1ab84522dea4d5c89299e
David <davidgcastro93@gmail.com>
2017-09-24 12:48:56 -0400

    still need to fix jquery bug

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,61 @@
 //for masonry
-
 var container = document.querySelector('#masonry');
-  var masonry = new Masonry(container, {
-    columnWidth: 5,
-    itemSelector: '.item'
-  });
-
-
+var masonry = new Masonry(container, {
+	columnWidth: 5,
+	itemSelector: '.item'
+});
 //Sidefilter logic
-$(document).ready(function(){
+$(document).ready(function() {
+	$(".sidefilter-item").on("click", function() {
+        //adds background to side filter items when clicked
+		$(this).addClass("side-filter-item_selected");
+        // adds an icon to the side filter item selected
+		$(this).find('.close').addClass("fa fa-times");
+		var chosenFilter = $(this).attr('id');
+        
+		
+        //loop thru images and hide elements that do not share the chosenFilter name
+		$("img").each(function() {
+			var currentImg = this;
+            
+            //hide img elements that DO NOT have the class of the sidefilter selected
+			if ($(currentImg).hasClass(chosenFilter)) {
+                $("."+chosenFilter).removeClass('hide');
+            	$(this).removeClass('hide');
+			}
+            else{
+				$(this).addClass('hide');
+            }
+          //remove hide of an image when the appropriate filter has been chosen.
+          //trigger masonry
+         
+		});
+        
+        $('.close').on('click', function(){
+             // chose side filter
+             var parent = $(this).parent().attr('id');
+             var currentImg = this;
+             $('img').each(function(){
+                 //target classes that have side filter class
+                 if(!$(this).hasClass(parent) && $(this).hasClass("hide") ){
+                   //make them appear
+                   $(this).removeClass('hide');
+                 }
+             });
 
-$( ".sidefilter-item" ).on( "click", function() {
-    $(this).addClass("side-filter-item_selected"); 	
-    $(this).find('.close').addClass("fa fa-times"); 
-    //add function to sort images
-    //adding click adds variable
-    //clicking on additional sidefilter items adds items to be filters
+        });
+       
+      
+        
+        
+	});
+        //displays active state and close icon
+    	$('.close').click(function(e) {
+			e.stopPropagation();
+            //removes icon when clicking icon
+			$(this).removeClass('fa fa-times');
+            // removes clicked state when clicking icon
+			$(this).parent().removeClass("side-filter-item_selected");
+   
+		});
 });
-
-//$('.sidefilter-item').on("click", '.close', function(){
-//    $(this).parent().removeClass('.side-filter-item_selected');
-//  
-//})
-
-$('.close').click(function(e){
-    //function to take elements out of filter
-            e.stopPropagation();
-            $(this).removeClass('fa fa-times');
-            $(this).parent().removeClass("side-filter-item_selected");
-                
-});
-
-
-});
-
-
-
-
-
-	 // var filter = $(this).attr('id');
-	 // $("img").addClass('hide');
-	 // setTimeout(function(){
-  //       $("." + filter).removeClass("hide");
-	 // },100);
