ce8a5d3f5543edbe7c81dde6f87be38e59bf60c8
David <davidgcastro93@gmail.com>
2017-09-26 23:35:39 -0400

    cannot fix js bug

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,10 @@ var masonry = new Masonry(container, {
 });
 //Sidefilter logic
 $(document).ready(function() {
+    
+    
+    
+    
 	$(".sidefilter-item").on("click", function() {
 		//adds background to side filter items when clicked
 		$(this).addClass("side-filter-item_selected");
@@ -13,39 +17,66 @@ $(document).ready(function() {
 		$(this).find('.close').addClass("fa fa-times");
 		var chosenFilter = $(this).attr('id');
 		//loop thru images and hide elements that do not share the chosenFilter name
-		
+	
         
         
         $('img').each(function() {
-			//target classes that have side filter class
+			//target img classes that have side filter class
 			if ($(this).hasClass(chosenFilter)) {
+                //add "chosen" to selected images
 				$(this).addClass('chosen');
+                $(this).removeClass('hide');
 			}
 			if ($(this).hasClass('chosen')) {
+                // if you have "chosen" class, unhide 
 				$(this).removeClass('hide');
 			} 
             else {
 				$(this).addClass('hide');
 			}
+            
 		});
 		//displays active state and close icon
 		$('.close').click(function(e) {
 			e.stopPropagation();
 			//removes icon when clicking icon
 			$(this).removeClass('fa fa-times');
-			// removes clicked state when clicking icon
+			// removes clicked state when clicking X icon
 			$(this).parent().removeClass("side-filter-item_selected");
+            //targets the filter where X is located
 			var parent = $(this).parent().attr('id');
-			
+            
+          
+           //loops thru images 
             $('img').each(function() {
-				if ($(this).hasClass(parent)) {
+                  var imgHidden =  $('.hide').get().length;
+                  //if you are the chosen filter and arent the only one visible
+                if ($(this).hasClass(parent)){
+                    //remove chosen class
 					$(this).removeClass('chosen');
-					$(this).addClass('hide');
+                    //and hide 	
+                    $(this).addClass('hide')
 				}
-              
-			
+                
+               if ($('.hide').get().length == $('img').length){
+                    unhide();
+                } 
+                
+//              
 			});
-		});
 		
+        });
 	});
+    
+    
+     function unhide(){
+        $('img').removeClass('hide');
+   
+
+  
+     }
+    
+    
+    
+    
 });
