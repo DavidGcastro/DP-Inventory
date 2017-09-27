5cba850989375eb733fd4da9cad77d721112718a
David <davidgcastro93@gmail.com>
2017-09-27 00:14:39 -0400

    fixed jquery bug

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,6 @@ var masonry = new Masonry(container, {
 });
 //Sidefilter logic
 $(document).ready(function() {
-    
-    
-    
-    
 	$(".sidefilter-item").on("click", function() {
 		//adds background to side filter items when clicked
 		$(this).addClass("side-filter-item_selected");
@@ -17,24 +13,19 @@ $(document).ready(function() {
 		$(this).find('.close').addClass("fa fa-times");
 		var chosenFilter = $(this).attr('id');
 		//loop thru images and hide elements that do not share the chosenFilter name
-	
-        
-        
-        $('img').each(function() {
+		$('img').each(function() {
 			//target img classes that have side filter class
 			if ($(this).hasClass(chosenFilter)) {
-                //add "chosen" to selected images
+				//add "chosen" to selected images
 				$(this).addClass('chosen');
-                $(this).removeClass('hide');
+				$(this).removeClass('hide');
 			}
 			if ($(this).hasClass('chosen')) {
-                // if you have "chosen" class, unhide 
+				// if you have "chosen" class, unhide 
 				$(this).removeClass('hide');
-			} 
-            else {
+			} else {
 				$(this).addClass('hide');
 			}
-            
 		});
 		//displays active state and close icon
 		$('.close').click(function(e) {
@@ -43,40 +34,30 @@ $(document).ready(function() {
 			$(this).removeClass('fa fa-times');
 			// removes clicked state when clicking X icon
 			$(this).parent().removeClass("side-filter-item_selected");
-            //targets the filter where X is located
+			//targets the filter where X is located
 			var parent = $(this).parent().attr('id');
-            
-          
-           //loops thru images 
-            $('img').each(function() {
-                  var imgHidden =  $('.hide').get().length;
-                  //if you are the chosen filter and arent the only one visible
-                if ($(this).hasClass(parent)){
-                    //remove chosen class
+			
+            //loops thru images 
+			$('img').each(function() {
+				var imgHidden = $('.hide').get().length;
+				//if you are the chosen filter and arent the only one visible
+				if ($(this).hasClass(parent)) {
+					//remove chosen class
 					$(this).removeClass('chosen');
-                    //and hide 	
-                    $(this).addClass('hide')
+					//and hide 	
+					$(this).addClass('hide')
 				}
-                
-               if ($('.hide').get().length == $('img').length){
-                    unhide();
-                } 
-                
-//              
-			});
-		
-        });
-	});
-    
-    
-     function unhide(){
-        $('img').removeClass('hide');
-   
+				if($('.chosen').length == 0){
+                     $('img').removeClass('hide');
 
+				}
+			             
+			});
+		});
+	});
+
+
+       
+        
   
-     }
-    
-    
-    
-    
 });
